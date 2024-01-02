8d6cdcace0c0ee2a30f6244b2512848ec36f5883
Bryan Avalos <trollerepik@gmail.com>
2024-01-02 15:16:49 -0600

    Feat: end of the project index

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				'sm': '320px',  // Cambia el valor según tus necesidades
+				'md': '640px',
+				'lg': '768px',
+				'xl': '1024px'
+			}
+		},
 	},
 	plugins: [],
 }
